95ffa2c258a7c9c56effe7a5f994974be6161df5
Amr Waheed <amrmohamed877@gmail.com>
2025-10-25 18:10:09 +0300

    ..

--- app/boards/[id]/BoardContent.tsx
+++ app/boards/[id]/BoardContent.tsx
@@ -1,18 +1,15 @@
 import { getTasksForBoard } from '@/app/services/actions/taskActions';
 import { getColumns } from '@/app/services/actions/columnActions';
 import BoardContentClient from './BoardContentClient';
-import { useMemo } from 'react';
 
 const BoardContent = async ({ id }: { id: string }) => {
   const tasks = await getTasksForBoard(id);
   const columns = await getColumns(id);
 
-  const columnsWithTasks = useMemo(() => {
-    return columns.map((column) => ({
-      ...column,
-      tasks: tasks.filter((task) => task.board_column_id === column.id),
-    }));
-  }, [tasks, columns]);
+  const columnsWithTasks = columns.map((column) => ({
+    ...column,
+    tasks: tasks.filter((task) => task.board_column_id === column.id),
+  }));
   return (
     <BoardContentClient
       id={id}
